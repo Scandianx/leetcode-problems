e560f1d75a5847afcdd8b5680c532a1cda3cfcd5
Scandianx <scandianidev@gmail.com>
2024-12-05 14:48:17 -0300

    first commit2

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class Permutation {
     public static void main(String[] args) throws Exception {
 
         Solution solution = new Solution();
